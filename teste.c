23cd257875fcf464503183fe0145a1c8042c6b73
BrunoMarini <bgmarini@hotmail.com>
2018-04-09 14:51:54 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -2,37 +2,37 @@
 #include <stdio.h>
 #include "matriz.h"
 
-//Funcao criarMatriz(linhas, colunas); > Pronta
-//Funcao destruitMatriz(*matriz);      > Pronta retorna 1 caso matriz nula retorna 2 caso desalocada com sucesso
-//Funcao void imprimirMatriz(float**); > Fazendo
+//Funcao float** criarMatriz(linhas, colunas);                    						> Pronta
+//Funcao void destruitMatriz(referencia, linhas);               						> Pronta 
+//Funcao void imprimirMatriz(ponteiro, linhas, colunas);   							> Pronta	
+//Funcao void atribuirValor(ponteiro, valor, linha, coluna, linhas, colunas)					> Pronta
+//Funcao void transporMatriz(ponteiro, ponteiro, linhas, colunas);
 
 int main()
 {
 	float **p;
-	int i, j, count = 5;
+	int i, j, count = 0;
+	int linhas = 3, colunas = 3;
 	
-	p = criarMatriz(3, 3);
+	p = criarMatriz(linhas, colunas);
 
-	for(i = 0; i < 3; i++){
-		for(j = 0; j < 3; j++){
+	for(i = 0; i < linhas; i++){
+		for(j = 0; j < colunas; j++){
 			p[i][j] = count;
-			count += 2;
+			count += 1;
 		}
 	}
-	
-	for(i = 0; i < 3; i++){
-		for(j = 0; j < 3; j++){
-			printf(" %.f ", p[i][j]);
-		}
-		printf("\n");
-	}
-	
-
 	
 		
-	//imprimirMatriz(p);
+	imprimirMatriz(p, linhas, colunas);
+	printf("\n\n\n");
+	//destruirMatriz(p, linhas);
 	
-	destruirMatriz(p);
+	atribuirValor(p, 9, 0, 2, linhas, colunas);
+	imprimirMatriz(p, linhas, colunas);
+
+
+	
 	
 	
 	
