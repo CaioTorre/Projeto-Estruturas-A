0c5329e3a394410a9e8a5ae03372b66ab97e2074
gabrielala10 <gabriela_m18@outlook.com>
2018-04-09 14:55:48 -0300

    Update lista.c

--- lista.c
+++ lista.c
@@ -4,17 +4,36 @@
 #define TAMANHO_NOME 20
 
 struct matriz{
-char nome [20];
+char nome [TAMANHO_NOME];
 int linha;
 int coluna
-float *mat;
+float **mat;
 matriz *prox;
 };
 
 void nova_matriz (char nome[TAMANHO_NOME], int linha, int coluna, matriz **inicio)
 {
 	matriz *aux;
-	aux=(*matriz)malloc(*matriz);
-	strcpy(aux->nome, nome);
+	aux = (*matriz) malloc (*matriz);
+	strcpy (aux->nome, nome);
+	aux->linha = linha;
+	aux->coluna = coluna;
+	//aux->*mat = criarMatriz (linha, coluna);
+	aux->prox = *inicio;
+	*inicio = aux;
+}
+
+void exclui_matriz (char nome[TAMANHO_NOME], matriz **inicio)
+{
+	matriz *aux = *inicio;
+	if(!aux){	
+		printf("ERRO\n");
+		break;
+		}
+	if (strcmp(nome, aux->prox->nome) == 0){
+
+	}
+	if (strcmp(nome, aux->prox->nome) != 0)
+		exclui_matriz (nome, &aux->prox);
 	
 }
