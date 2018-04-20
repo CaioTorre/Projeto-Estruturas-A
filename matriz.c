2c362cb133266b602fcaf0d89badf772e51f4b3c
marcosatsf <marcos_atsf@hotmail.com>
2018-04-20 16:16:57 -0300

    EOF == (matriz.c && matriz.h)

--- matriz.c
+++ matriz.c
@@ -1,21 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-float **criarMatriz(int linhas, int colunas)
+float **criarMatriz(int linhas, int colunas, int zera)
 {
 	float **Matriz;
 	
 	if(linhas >= 50 || linhas <= 1 || colunas >= 50 || linhas <= 1)
 	{
 		printf("ERRO\n");
+		return (-1);
 	}
 	else
 	{
-		Matriz = (float **)malloc(linhas * sizeof(float));
-		for(int i=0; i < linhas; i++)
-		{
-			Matriz[i] = (float *)malloc(colunas * sizeof(float));
+		if(zera){
+			if(!(Matriz = (float **)calloc(linhas,sizeof(float)))) return (-2);
+		
+			for(int i=0; i < linhas; i++)
+			{
+				Matriz[i] = (float *)calloc(colunas, sizeof(float));
+			}
 		}
+		else{
+			if(!(Matriz = (float **)malloc(linhas * sizeof(float)))) return (-2);
+			
+			for(int i=0; i < linhas; i++)
+			{
+				Matriz[i] = (float *)malloc(colunas * sizeof(float));
+			}
+		} 
 
 		if(!Matriz)
 		{
@@ -71,12 +83,97 @@ void atribuirValor(float **matriz, float valor, int linha, int coluna, int linha
 
 float** transporMatriz(float **matriz, int linhas, int colunas)
 {
-	int i, j;
+	if(!matriz){
+		printf("ERRO\n");
+		return -1;
+	}
+	float **aux = criarMatriz(colunas, linhas, 0);
+	for(int i = 0; i < linhas; i++){
+		for(int j = 0; j < colunas; j++){
+			aux[j][i] = matriz[i][j];
+			printf("%6.2f", aux[j][i]);
+		}
+	printf("\n");		
+	}
+	return aux; 
 		
-
-	criarmatriz
-	
 }
+float**  somarMatriz(float **mat1, float **mat2, int lin1, int col1, int lin2, int col2){
+	if((!mat1 || !mat2) || ((lin1 != lin2) && (col1 != col2))){
+		printf("ERRO\n");
+		return -1;
+	}
+	else{
+		float **aux = criarMatriz(lin1, col1, 0);
+		for(int i=0; i < lin1; i++){
+			for(int j=0; j < col1; j++){
+				aux[i][j] = mat1[i][j]+mat2[i][j];
+				printf("%6.2f", aux[i][j]);
+			}
+			printf("\n");
+		}
+		return aux;
+	}
+}
+float**  divideMatriz(float **mat1, float **mat2, int lin1, int col1, int lin2, int col2){
+	if((!mat1 || !mat2) || ((lin1 != lin2) && (col1 != col2))){
+		printf("ERRO\n");
+		return -1;
+	}
+	else{
+		for(int i=0; i<lin2;i++)
+			for(int j=0; j<col2;j++)
+				if(mat2[i][j] == 0){
+					printf("ERRO\n");
+					return -2;
+				}
+		float **aux = criarMatriz(lin1, col1,0);
+		for(int i=0; i<lin2;i++){
+			for(int j=0; j<col2;j++){
+				aux[i][j] = mat1[i][j]/mat2[i][j];
+				printf("%6.2f", aux[i][j]);
+				}
+			printf("\n");
+			}
+		return aux;
+	}
+}
+float**  ME(float **mat1, float **mat2, int lin1, int col1, int lin2, int col2){
+	if((!mat1 || !mat2) || ((lin1 != lin2) && (col1 != col2))){
+		printf("ERRO\n");
+		return -1;
+	}
+	else{
+		float **aux = criarMatriz(lin1, col1,0);
+		for(int i=0; i<lin2;i++){
+			for(int j=0; j<col2;j++){
+				aux[i][j] = mat1[i][j]*mat2[i][j];
+				printf("%6.2f", aux[i][j]);
+				}
+			printf("\n");
+			}
+		return aux;
+	}
+}
+float**  MM(float **mat1, float **mat2, int lin1, int col1, int lin2, int col2){
+	if((!mat1 || !mat2) || (col1 != lin2)){
+		printf("ERRO\n");
+		return -1;
+	}
+	else{
+		float **aux = criarMatriz(lin1, col2,1);
+		for(int i=0; i<col1;i++){
+			for(int t=0; t<col2; t++){
+				for(int j=0; j<lin2;j++) aux[i][t]+=mat1[i][j]*mat2[j][i+t];
+				printf("%6.2f", aux[i][t]);
+			}
+		printf("\n");
+		}
+		return aux;
+	}
+}
+
+
 
 
 
--- matriz.h
+++ matriz.h
@@ -1,9 +1,11 @@
-float** criarMatriz(int linhas, int colunas);
+float** criarMatriz(int linhas, int colunas, int zera);
 void destruirMatriz(float **matriz, int linhas);
 void imprimirMatriz(float **matriz, int linhas, int colunas);
 void atribuirValor(float **matriz, float valor, int linha, int coluna, int linhas, int colunas);
-float** transporMatriz(float **matriz, float **transposta, int linhas, int colunas);
-
-
+float** transporMatriz(float **matriz, int linhas, int colunas);
+float**  somarMatriz(float **mat1, float **mat2, int lin1, int col1, int lin2, int col2);
+float**  divideMatriz(float **mat1, float **mat2, int lin1, int col1, int lin2, int col2);
+float**  MM(float **mat1, float **mat2, int lin1, int col1, int lin2, int col2);
+float**  ME(float **mat1, float **mat2, int lin1, int col1, int lin2, int col2);
 
 
