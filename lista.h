c44720c23a4255576d183d2045a5074b9322706b
gabrielala10 <gabriela_m18@outlook.com>
2018-04-23 14:44:34 -0300

    Todas as funções

--- lista.h
+++ lista.h
@@ -1,2 +1,12 @@
-void nova_matriz (char nome[20], int linha, int coluna);
-void exclui_matriz (char nome[20]);
+void nova_matriz (char nome[20], int linhas, int colunas);
+void exclui_matriz (char nome[], matriz **inicio);
+void imprime_matriz (char nome1[], nome2[], nome[], matriz *inicio);
+void atribui_valor (char nome[TAMANHO_NOME], matriz *inicio, float valor, int linha, int coluna);
+void atribui_linha (char nome[], matriz *inicio, int linha);
+void atribui_coluna (char nome[], matriz *inicio, int coluna);
+void transpor_matriz (char nome[], matriz *inicio);
+void somar_matriz (char nome1[], char nome2[], char nome[], matriz **inicio);
+void dividir_matriz (char nome1[], char nome2[], char nome[], matriz **inicio);
+void ME (char nome1[], char nome2[], char nome[], matriz **inicio);
+void MM (char nome1[], char nome2[], char nome[], matriz **inicio);
+matriz *encontra_matriz (char nome [TAMANHO_NOME], matriz *inicio);
